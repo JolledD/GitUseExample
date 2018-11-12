280c8fcc0d7d623bd832cbe85660f1f251f9165a
Jolley, Dallin P <201613@s020rmc148-08.local>
2018-11-12 12:02:45 -0600

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Jolley, Dallin P on 11/12/18.
+//  Copyright © 2018 Jolley, Dallin P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
