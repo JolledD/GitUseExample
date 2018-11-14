5fd65a2fb7663bd6c0269abf27848c9961c769e3
Jolley, Dallin P <201613@s020rmc148-08.local>
2018-11-14 11:03:19 -0600

    commit

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,9 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Jolley, Dallin P on 11/14/18.
+//  Copyright © 2018 Jolley, Dallin P. All rights reserved.
+//
+
+import Foundation
