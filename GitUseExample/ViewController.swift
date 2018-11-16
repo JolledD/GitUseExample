7dc381f9b33afd547e083309057ed73fca3b2a5f
Jolley, Dallin P <201613@s020rmc148-08.local>
2018-11-16 11:22:16 -0600

    finished message on button press

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
+    @IBAction func buttonClicked(_sender: UIButton){
+        print("You're finidhed")
+    }
 
 }
 
